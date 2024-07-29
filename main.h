68c94ef31eb35f8cdf8856b0f6a2e0a60982e4e2
BettyNyambura <btynyambura20@gmail.com>
2024-07-29 05:36:29 -0700

    additional files

--- main.h
+++ main.h
@@ -1,15 +1,33 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <stddef.h>
 
-int _putChar(char c);
+
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list);
+} specifiers_t;
+
+/*prototypes*/
 int _printf(const char *format, ...);
-int print_int(va_list list_of_arg);
+int get_function(char s, va_list args);
+int _putchar(char c);
+
+/*Conversion specifiers*/
 int print_char(va_list args);
 int print_string(va_list args);
-int print_percent(va_list args);
+int print_digit(va_list args);
+int print_mod(va_list args);
+int print_rev_string(va_list args);
 
 #endif
