e4a1084decc077c8423c3fe98b41e29269f353eb
BettyNyambura <btynyambura20@gmail.com>
2024-07-31 09:53:25 -0700

    binary correction

--- print_binary.c
+++ print_binary.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_binary - prints an integer in binary format
  * @arg: argument to print
  * Return: number of characters printed
  */
+
 int print_binary(va_list arg)
 {
 	unsigned int n = va_arg(arg, unsigned int);
@@ -12,25 +14,25 @@ int print_binary(va_list arg)
 
 	if (n == 0)
 	{
-		putchar('0');
-		len++;
+	putChar('0');
+	len++;
 	}
 	else
 	{
-		while (n > 0)
-		{
-			binary[i] = n % 2;
-			n /= 2;
-			i++;
-		}
-		i--;
-		while (i >= 0)
-		{
-			putchar(binary[i] + '0');
-			len++;
-			i--;
-		}
+	while (n > 0)
+	{
+	binary[i] = n % 2;
+	n /= 2;
+	i++;
 	}
+	i--;
+        while (i >= 0)
+        {
+	putChar(binary[i] + '0');
+	len++;
+	i--;
+	}
+}
 
-	return (len);
+    return (len);
 }
