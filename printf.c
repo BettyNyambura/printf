e2156eb1fdf8dd44ea25566ed737ea501cee252f
BettyNyambura <btynyambura20@gmail.com>
2024-07-31 03:13:33 -0700

    adding handle-f-rmat funtion

--- printf.c
+++ printf.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
+int handle_format(const char *format, va_list args, int count);
+
 /**
  * _printf - produces output according to format.
  * @format: character string
  *
  * Return: number of characters.
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -14,35 +16,13 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
+
 	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-				case 'b':
-					count += print_binary(args);
-					break;
-				case 'c':
-					count += print_character(args);
-					break;
-				case '%':
-					count += print_percent(args);
-					break;
-				case 's':
-					count += print_string(args);
-					break;
-				case 'i':
-					count += print_number(args);
-					break;
-				case 'd':
-					count += print_number(args);
-					break;
-				default:
-					_printDefault(args, format);
-					count += 2;
-			}
+			count = handle_format(format, args, count);
 		}
 		else
 		{
@@ -52,5 +32,42 @@ int _printf(const char *format, ...)
 		format++;
 	}
 	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_format - handles the format specifiers
+ * @format: format string
+ * @args: argument list
+ * @count: current count of printed characters
+ *
+ * Return: updated count of printed characters
+ */
+int handle_format(const char *format, va_list args, int count)
+{
+	switch (*format)
+	{
+		case 'b':
+			count += print_binary(args);
+			break;
+		case 'c':
+			count += print_character(args);
+			break;
+		case '%':
+			count += print_percent(args);
+			break;
+		case 's':
+			count += print_string(args);
+			break;
+		case 'i':
+			count += print_number(args);
+			break;
+		case 'd':
+			count += print_number(args);
+			break;
+		default:
+			_printDefault(args, format);
+			count += 2;
+	}
 	return (count);
 }
